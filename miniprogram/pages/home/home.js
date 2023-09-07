ad46c2c3a40c569064ce8d2d61680ceed062985b
yangmanbao <15738345848@163.com>
2023-09-07 18:52:54 +0800

    首页样式完成

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -11,71 +11,130 @@ Page({
     menuRight: app.globalData.menuRight,
     menuBottom: app.globalData.menuBottom,
     menuHeight: app.globalData.menuHeight,
-    list:{// 表格标题
-      th: ["排名", "头像","模型名称","恐模分"],// 表格内容  这里只能使用 数组套数组格式
-      td: [
-        ["1","10","霸王龙威尔逊","7.5"],
-        ["2","9","霸王龙威尔逊","7.5"],
-        ["3","8","霸王龙威尔逊","7.5"],
-        ["4","7","霸王龙威尔逊","7.5"],
-      ],}
+    listdata: [
+      {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      },
+      {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      }, {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      }, {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      }, {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      }, {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      }, {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      }, {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      }, {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      }, {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      }, {
+        "ranking": "1",
+        "arter": 'https://img.yzcdn.cn/vant/cat.jpeg',
+        "brandName": "霸王龙威尔逊",
+        "score": "7.5"
+      },
+    ],
+    showColor:false,
   },
+  //点击模型跳转到对应页面
 
+  detailPage() {
+    wx.navigateTo({
+      url: '/pages/detailsPage/detailsPage'
+    })
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  // onLoad(options) {
+
+  // },
+  onLoad: {
+   
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
 
   },
-  onLoad: function (options) {
-    
-        this.setData({list})},
-      /**
-       * 生命周期函数--监听页面初次渲染完成
-       */
-      onReady() {
 
-      },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
 
-      /**
-       * 生命周期函数--监听页面显示
-       */
-      onShow() {
+  },
 
-      },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
 
-      /**
-       * 生命周期函数--监听页面隐藏
-       */
-      onHide() {
+  },
 
-      },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
 
-      /**
-       * 生命周期函数--监听页面卸载
-       */
-      onUnload() {
+  },
 
-      },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
 
-      /**
-       * 页面相关事件处理函数--监听用户下拉动作
-       */
-      onPullDownRefresh() {
+  },
 
-      },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
 
-      /**
-       * 页面上拉触底事件的处理函数
-       */
-      onReachBottom() {
+  },
 
-      },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
 
-      /**
-       * 用户点击右上角分享
-       */
-      onShareAppMessage() {
-
-      }
-    })
+  }
+})
